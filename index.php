b7774367bf850413763a6050c1b19548c04860c4
EnzoPierucci <enzopierucci@gmail.com>
2024-02-27 15:28:24 +0100

    file

---
+++ index.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="https://cdn.tailwindcss.com" rel="stylesheet">
+    <title>php-strong-password-generator</title>
+</head>
+<body>
+
+<script src="https://cdn.tailwindcss.com?plugins=forms,typography,aspect-ratio,line-clamp"></script>
+</body>
+</html>
