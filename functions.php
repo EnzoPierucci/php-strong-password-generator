894cf075d8072ee490193e64f6fcd32487fe9ad2
EnzoPierucci <enzopierucci@gmail.com>
2024-02-27 16:01:56 +0100

    milestone 2

---
+++ functions.php
@@ -0,0 +1,11 @@
+<?php
+// Funzione per generare la password
+function generatePassword($length = 8) {
+    $chars = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789!@#$%&()_+-=[]{}?';
+    $password = '';
+    for ($i = 0; $i < $length; $i++) {
+        $password .= $chars[rand(0, strlen($chars) - 1)];
+    }
+    return $password;
+}
+?>
--- index.php
+++ index.php
@@ -1,20 +1,11 @@
 <?php
-// Funzione per generare la password
-function generatePassword($length = 8) {
-    $chars = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789!@#$%&()_+-=[]{}?';
-    $password = '';
-    for ($i = 0; $i < $length; $i++) {
-        $password .= $chars[rand(0, strlen($chars) - 1)];
-    }
-    return $password;
-}
+require 'functions.php'; // collegamento a  functions
 
-$password = '';
+$password = ''; 
 if (isset($_GET['length']) && is_numeric($_GET['length'])) {
     $password = generatePassword((int)$_GET['length']);
 }
 ?>
-
 
 <!DOCTYPE html>
 <html lang="en">
@@ -28,14 +19,13 @@ if (isset($_GET['length']) && is_numeric($_GET['length'])) {
 <body>
 <form action="index.php" method="GET">
         <label for="length">Lunghezza Password:</label>
-        <input type="number" id="length" name="length" required> <!-- requied per renderlo obbligatorio --> 
+        <input type="number" id="length" name="length" required>  <!-- requied per renderlo obbligatorio --> 
         <input type="submit" value="Genera Password">
     </form>
 
     <?php if ($password): ?>
         <p>Password Generata: <strong><?= htmlspecialchars($password) ?></strong></p>
     <?php endif; ?>    
-    
     <script src="https://cdn.tailwindcss.com?plugins=forms,typography,aspect-ratio,line-clamp"></script>
 </body>
 
