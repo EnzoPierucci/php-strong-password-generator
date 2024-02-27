8cbf8d03ee3ac4d13f33dd855edd8c78a481c337
EnzoPierucci <enzopierucci@gmail.com>
2024-02-27 15:45:37 +0100

    milestone 1

--- index.php
+++ index.php
@@ -1,3 +1,21 @@
+<?php
+// Funzione per generare la password
+function generatePassword($length = 8) {
+    $chars = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789!@#$%&()_+-=[]{}?';
+    $password = '';
+    for ($i = 0; $i < $length; $i++) {
+        $password .= $chars[rand(0, strlen($chars) - 1)];
+    }
+    return $password;
+}
+
+$password = '';
+if (isset($_GET['length']) && is_numeric($_GET['length'])) {
+    $password = generatePassword((int)$_GET['length']);
+}
+?>
+
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -6,8 +24,19 @@
     <link href="https://cdn.tailwindcss.com" rel="stylesheet">
     <title>php-strong-password-generator</title>
 </head>
-<body>
 
-<script src="https://cdn.tailwindcss.com?plugins=forms,typography,aspect-ratio,line-clamp"></script>
+<body>
+<form action="index.php" method="GET">
+        <label for="length">Lunghezza Password:</label>
+        <input type="number" id="length" name="length" required> <!-- requied per renderlo obbligatorio --> 
+        <input type="submit" value="Genera Password">
+    </form>
+
+    <?php if ($password): ?>
+        <p>Password Generata: <strong><?= htmlspecialchars($password) ?></strong></p>
+    <?php endif; ?>    
+    
+    <script src="https://cdn.tailwindcss.com?plugins=forms,typography,aspect-ratio,line-clamp"></script>
 </body>
+
 </html>
